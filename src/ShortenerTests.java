1f0d6d2a1c82e18fbb66a6362a27b786f2d49dd2
Calvyn Siong <calvyn360@gmail.com>
2025-08-05 09:58:32 -0400

    Add tests

--- src/ShortenerTests.java
+++ src/ShortenerTests.java
@@ -1,2 +1,24 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ShortenerTests {
+    @Test
+    public void testShortenTwoCharacters() throws EmptyStringException {
+        // Arrange
+        String input = "ab";
+        String expected = "";
+        // unit under test
+        Truncator uut = new Truncator();
+
+        // Act
+        String actual = uut.truncate(input);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void AwfulTestShortenTwoCharacters() throws EmptyStringException {
+        Truncator uut = new Truncator();
+        assertEquals("", uut.truncate("ab"));
+    }
 }
